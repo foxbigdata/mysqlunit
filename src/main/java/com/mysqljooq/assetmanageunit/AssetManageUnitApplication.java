ed5007d7a8aef9cb50bb364bce8a14f33684c1de
李锐 <lirui@36kr.com>
2019-03-03 13:41:17 +0800

    【jooq连接mysql作单元测试】

---
+++ src/main/java/com/mysqljooq/assetmanageunit/AssetManageUnitApplication.java
@@ -0,0 +1,13 @@
+package com.mysqljooq.assetmanageunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManageUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManageUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysqljooq/assetmanageunit/AssetManageUnitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mysqljooq.assetmanageunit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AssetManageUnitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
